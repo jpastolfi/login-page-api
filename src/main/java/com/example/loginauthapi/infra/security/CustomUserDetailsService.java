3a08edea06cb4af7960aea629ca284999701c99c
Joao Astolfi <jpastolfi@gmail.com>
2024-05-04 00:38:09 +0100

    feat: created SecurityConfig and UserDetailsService (included check for existing email)

---
+++ src/main/java/com/example/loginauthapi/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.example.loginauthapi.infra.security;
+
+import com.example.loginauthapi.domain.user.User;
+import com.example.loginauthapi.repositories.UserRepository;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+  @Autowired
+  private UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+  }
+}
